ea346c5f06f105fca6cc2e853629009c87fe45d9
jaywood <jay2plugish.com>
2014-07-10 00:34:35 -0400

    Version numbers

--- content-warning-v3.php
+++ content-warning-v3.php
@@ -4,7 +4,7 @@ Plugin Name:	Content Warning v2
 Plugin URI:		http://plugish.com/plugins/content-warning-v3
 Description: 	A plugin based on my v2 code, while drastically deviating from the original.  Used mainly for NSFW websites, this plugin provides a dialog popup to warn viewers of it's possible content.
 Author: 		Jerry Wood Jr.
-Version:		3.48
+Version:		3.49
 Author URI:		http://plugish.com
 */
 require_once (dirname(__FILE__).'/inc/options.inc.php');
@@ -12,4 +12,3 @@ require_once (dirname(__FILE__).'/lib/jw_simple_options/simple_options.php');
 require_once (dirname(__FILE__).'/class/main.class.php');
 $cwv3_options = new JW_SIMPLE_OPTIONS($cwv3_op_data);
 register_uninstall_hook(__FILE__, $cwv3_options->uninstall() );
-?>
