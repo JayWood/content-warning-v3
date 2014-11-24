058cbef9f58fc7ff573388782673f4c566fcca4e
Jay Wood <jerry@webdevstudios.com>
2014-11-24 09:47:24 -0500

    Fix enter/exit message defaults

--- inc/api.php
+++ inc/api.php
@@ -28,7 +28,7 @@ function cwv3_get_css(){
 	?>
 	<!-- CWV3 CSS -->
 	<style type="text/css">
-	#cboxOverlay{<?php echo $bg_image_css . $bg_color_css; ?>}
+	.cwv3.dialog-overlay{<?php echo $bg_image_css . $bg_color_css; ?>}
 	<?php
 	if ( ! empty( $custom_css ) ){
 		echo apply_filters( 'cwv3_custom_css', $custom_css );
@@ -54,8 +54,8 @@ function cwv3_get_js_dialog(){
 	$cwv3_denial_title   = get_option( 'cwv3_den_title', __( 'Access Denied', 'cwv3' ) );
 	$cwv3_denial_message = get_option( 'cwv3_den_msg', __( 'You have been denied access to this content.  If you feel this is in error, please contact a site administrator.', 'cwv3' ) );
 
-	$cwv3_title          = get_option( 'cwv3_den_title', __( 'Access Denied', 'cwv3' ) );
-	$cwv3_message        = get_option( 'cwv3_den_msg', __( 'The content you are about to view may be considered offensive and/or inappropriate.  Furthermore, this content may be considered adult content, if you are not of legal age or are easily offended, you are required to click the exit button.', 'cwv3' ) );
+	$cwv3_title          = get_option( 'cwv3_d_title', __( 'Warning: Explicit Content', 'cwv3' ) );
+	$cwv3_message        = get_option( 'cwv3_d_msg', __( 'The content you are about to view may be considered offensive and/or inappropriate.  Furthermore, this content may be considered adult content, if you are not of legal age or are easily offended, you are required to click the exit button.', 'cwv3' ) );
 
 	$exit_url            = get_option( 'cwv3_exit_link', '#' );
 	$enter_url           = get_option( 'cwv3_enter_link', '#' );
@@ -63,6 +63,7 @@ function cwv3_get_js_dialog(){
 	ob_start();
 	?>
 	<!-- CWV3 JS Dialog -->
+	<div class="cwv3 dialog-overlay">&nbsp;</div>
 	<div id="cwv3_dialog" class="cwv3_dialog js">
 		<div class="cwv3 auth">
 			<div class="cwv3_title"><?php echo esc_attr( $cwv3_title ); ?></div>
