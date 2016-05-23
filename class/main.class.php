941b9290776bf3a989be0806e7eff862513f16bf
Jay Wood <jerry@webdevstudios.com>
2016-05-23 10:18:55 -0400

    Switch to use COOKIEPATH constant instead of SITECOOKIEPATH - cloases #42

--- class/main.class.php
+++ class/main.class.php
@@ -170,7 +170,7 @@ class CWV3 {
 		$de           = get_option( 'cwv3_denial', 'enabled' );
 		$localized_data = array(
 			'opacity'        => get_option( 'cwv3_bg_opacity', 0.85 ),
-			'cookie_path'    => SITECOOKIEPATH,
+			'cookie_path'    => COOKIEPATH,
 			'cookie_name'    => $this->get_cookie_name(),
 			'cookie_time'    => intval( $cookie_death ) > 365 ? 365 : intval( $cookie_death ),
 			// Max at one year if it's over 365 days.
